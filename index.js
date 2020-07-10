6e324e029743733ced780e23615b403733937178
JamesHawkinss <jhawki2005@gmail.com>
2020-07-10 18:36:03 +0100

    Webhooks!
    - changes avatar to author of message
    - changes username to author of message's nickname (if exists) else username
    - thank you jvs for help with promises n shit

--- index.js
+++ index.js
@@ -2,15 +2,44 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config');
 
+const webhooks = []
+
 function sendNewMessage(message) {
-    Object.keys(config.sync).forEach((key) => {
-        if (config.sync[key] == message.channel.id) return;
-        const channelToSend = client.guilds.cache.get(key).channels.cache.get(config.sync[key]);
-        channelToSend.send(`**${message.guild.name}** (#${message.channel.name} - ${message.member.nickname})\n${message.content}`)
+    webhooks.forEach((hook) => {
+        if (hook.channelID == message.channel.id) return;
+        const avatarURL = message.author.avatarURL();
+        hook.send(`**From ${message.guild.name}**\n${message.content}`, {
+            username: message.member.nickname,
+            avatarURL
+        });
     });
 }
 
-client.on('message', (message) => {
+async function getWebhook(name, channelId) {
+    const hooks = await client.channels.cache.get(channelId).fetchWebhooks();
+    const hook = hooks.find(hook => hook.name == name);
+    return hook;
+}
+
+async function createWebhooks() {
+    Object.keys(config.sync).forEach(async (key) => {
+        const hook = await getWebhook("Channel Sync", config.sync[key]);
+        if (!Boolean(hook)) {
+            client.channels.cache.get(config.sync[key]).createWebhook('Channel Sync', {
+                avatar: 'https://cdn.discordapp.com/embed/avatars/1.png',
+                reason: 'Created by Channel Sync'
+            })
+            .then((webhook) => {
+                webhooks.push(webhook);
+            })
+            .catch((err) => console.log(err));
+        } else {
+            webhooks.push(hook);
+        }
+    });
+}
+
+client.on('message', async (message) => {
     if (message.author.bot) return;
 
     if (config.sync[message.guild.id] == message.channel.id) {
@@ -18,8 +47,9 @@ client.on('message', (message) => {
     } else return;
 });
 
-client.on('ready', () => {
-    console.log('ready');
+client.on('ready', async () => {
+    console.log('bot logged in');
+    await createWebhooks();
 });
 
 client.login(config.discord.token);
